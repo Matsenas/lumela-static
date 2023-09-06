a76805ac745e40f1127242d030bc26712dafd147
Andrius Matsenas <mandrius.101@gmail.com>
2023-09-06 05:58:27 +0300

    added fadein to footer

--- src/lib/Footer.svelte
+++ src/lib/Footer.svelte
@@ -12,6 +12,11 @@
         width: 100%;
         will-change: transform;
         transition: transform 500ms ease-out;
+        -webkit-animation: fadein 3s; /* Safari, Chrome and Opera > 12.1 */
+        -moz-animation: fadein 3s; /* Firefox < 16 */
+        -ms-animation: fadein 3s; /* Internet Explorer */
+        -o-animation: fadein 3s; /* Opera < 12.1 */
+        animation: fadein 3s;
         p {
             margin: 0;
         }
